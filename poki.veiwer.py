36e239370b00e34ac7c78dcfda4dab393eb4e9a9
Abdullamahfouz <abdullah.mahfouz@flemingcollege.ca>
2023-04-06 18:58:36 -0400

    updated script

--- poki.veiwer.py
+++ poki.veiwer.py
@@ -4,43 +4,51 @@ from tkinter import *
 from tkinter import ttk
 import os
 import ctypes
+from image_lib import set_desktop_background_image
 
-def main():
-    return
+def get_background ():
+    background = set_desktop_background_image(image_path)
+    return background
 
-
+#gets the path of the script
 script_path = os.path.abspath(__file__)
 script_dir = os.path.dirname(script_path)
-image_cahe_dir = os.path(script_dir, 'images')
+image_cahe_dir = os.path.join(script_dir, 'images')
 if not os.path.isdir(image_cahe_dir):
     os.makedirs(image_cahe_dir)
-
+# makes the path
 root = Tk()
 root.title("Pokemon Image Viewer")
-root.minsize(600, 700)
+root.minsize(700, 700)
 
-
+# sets the pokemon weindow icon
 ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('COMP593.PokeImageViewer')
 icon_path = os.path.join(script_dir, 'Great-Ball.ico')
 root.iconbitmap(icon_path)
-
-frame = ttk.Frame(root, relief='ridge')
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+#creats the fra,e
+frame = ttk.Frame(root)
 frame.grid(row=0, column=0, padx=10 , pady=10, sticky=NSEW)
 frame.columnconfigure(0, weight=1)
 frame.rowconfigure (0, weight=1)
 
-img_poke = PhotoImage(file=os.path.join(script_dir))
+#Adds Image to frame
+img_poke = PhotoImage(file=os.path.join(script_dir, 'pokemon_logo.png'))
 lbl_poke_image = ttk.Label(frame, image=img_poke)
 lbl_poke_image.grid(row=0, column=0)
 
-pokemon_name_list = poki_api.get_pokemon_names()
-cbox_poke_names = ttk.Label(frame, values=pokemon_name_list, state='readonly')
+# Add the Pokemon names pull-down list to the frame
+pokemon_name_list = sorted(poki_api.get_pokemon_names())
+cbox_poke_names = ttk.Combobox(frame, values=pokemon_name_list, state='readonly')
 cbox_poke_names.set("Select a Pokemon")
-cbox_poke_names.grid(row=2, column=0, padx=10, pady=10)
+cbox_poke_names.grid(row=1, column=0, padx=10, pady=10)
 
 def handle_pokemon_sel(event):
-    
+    # Get the name of the selected Pokemon
     pokemon_name = cbox_poke_names.get()
+   
+    # Download and save the artwork for the selected Pokemon
     global image_path
     image_path = poki_api.get_pokemon_info(pokemon_name, image_cahe_dir )
     
@@ -48,14 +56,16 @@ def handle_pokemon_sel(event):
     if image_path is not None :
         img_poke['file'] = img_poke
         
+cbox_poke_names.bind('<<CombobxSelected>>', handle_pokemon_sel)
 
 
-
-
-
-
+# sets a button
 btn_set_desktop = ttk.Button(frame, text= ' Set as Desktop Image')
 btn_set_desktop.grid(row=2, column=0, padx=10, pady=10)
+
+
+
+
 
 
 root.mainloop()
@@ -64,5 +74,3 @@ root.mainloop()
 
 
 
-if __name__ == '__main__':
-    main()
--- poki_api.py
+++ poki_api.py
@@ -8,6 +8,7 @@ POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
 def main():
     
+ names = get_pokemon_names()
     
     
  get_pokemon_image('dugtrio',
@@ -59,15 +60,15 @@ def get_pokemon_names(offest=0, limit=100000):
   quer_str_params= {
       'offset': offest,
       'limit' : limit
-    }
+      }
 
 
   resp_msg = requests.get(POKE_API_URL, params=quer_str_params)
   
   if resp_msg.status_code == requests.codes.ok:
+      print('success ')
       pokemon_dict = resp_msg.json()
-      
-      poke_names_list = [p['names'] for p in pokemon_dict['results']]
+      poke_names_list = [p['name'] for p in pokemon_dict['results']]
       
       return poke_names_list
   else:
@@ -75,21 +76,21 @@ def get_pokemon_names(offest=0, limit=100000):
       print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
       return
 def get_pokemon_image(pokemon_name, save_dir):
-    
+    #Get all info for the specified pokemon
     pokemon_info = get_pokemon_info(pokemon_name)
     if pokemon_info is None :
         return
     artwork_url = pokemon_info['sprites']['other']['official-artwork']['front_default']
     
+    #the image file path
     image_bytes = image_lib.download_image(artwork_url)
     if image_bytes is None:
         return
     file_ext = artwork_url.split('.')[-1]
     image_path = os.path.join(save_dir, f'{pokemon_name}.{file_ext}' )
+    # save the image file
     
-    image_lib.save_image_file(image_bytes, image_path )
-   
-   
-    return
+    if image_lib.save_image_file(image_bytes, image_path ):
+        return image_path
 if __name__ == '__main__':
     main()
